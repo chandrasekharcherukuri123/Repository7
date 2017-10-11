31ae7180ef81d722bbfe6e1ef0ec793d0d3ab8ad
chandrasekhar009 <31537403+chandrasekhar009@users.noreply.github.com>
2017-10-11 15:24:40 +0530

    commit

---
+++ src/package5/Class6.java
@@ -0,0 +1,18 @@
+package package5;
+
+import java.util.Scanner;
+
+public class Class6 {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter username");
+		String u=sc.nextLine();
+		System.out.println("enter password");
+		String p=sc.nextLine();
+		
+
+	}
+
+}
